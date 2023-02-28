b37c805574f326f49d0439cd6d9ecb9c0110ea36
kliker <danilahreno@gmail.com>
2023-02-28 21:58:36 +0300

    Slider and Music

---
+++ Interface.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Numerics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+using System.Diagnostics;
+using System.Windows;
+
+namespace MusicPlayer
+{
+    internal class Interface
+    {
+        public static List<string> currentFiles = new List<string>();
+        public static MediaPlayer player = new MediaPlayer();
+        public static int lastIndex;
+        public static List<string> Open()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog { IsFolderPicker = true };
+            var result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                string[] files = Directory.GetFiles(dialog.FileName);
+                List<string> currentPaths = new List<string>();
+                List<string> currentNames = new List<string>();
+
+
+                foreach (string file in files)
+                {
+                    string ext = System.IO.Path.GetExtension(file);
+                    if (ext == ".mp3")
+                    {
+                        currentPaths.Add(file);
+                        string[] name = file.Split(new string[] { "\\" }, StringSplitOptions.None);
+                        currentNames.Add(name[name.Length - 1]);
+                    }
+                }
+                currentFiles = currentPaths;
+                return currentNames;
+            }
+            return null;
+        }
+
+        public static void Play(int ind, TimeSpan? PositionTimeMedia = null)
+        {
+            lastIndex = ind;
+            player.Open(new Uri(currentFiles[ind].Replace(@"\", @"\\").ToString(), UriKind.Absolute));
+            player.Position = (TimeSpan)(PositionTimeMedia == null ? TimeSpan.Zero : PositionTimeMedia);
+            player.Play();
+            Debug.WriteLine(currentFiles[ind].Replace(@"\", @"\\"));
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +26,65 @@ namespace MusicPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool start = false;
         public MainWindow()
         {
+
+            Interface.player.MediaEnded += Player_MediaEnded;
+            Interface.player.MediaOpened += Player_MediaOpened;
             InitializeComponent();
+            Interface.player.Volume = Sound.Value / 100;
+            Thread th = new Thread(_ =>
+            {
+                while (true)
+                {
+                    while (start)
+                    {
+                        Dispatcher.Invoke((Action)(() =>
+                        {
+                            SlideMusic.Value = (double)Interface.player.Position.TotalSeconds;
+                            Timer.Content = $"{(int)Interface.player.Position.Minutes}:{Interface.player.Position.Seconds:00}";
+                            Debug.WriteLine("sdsadasd");
+                        }));
+                        Thread.Sleep(10);
+                    }
+                }
+            });
+            th.Start();
         }
-    }
+
+        private void Player_MediaOpened(object? sender, EventArgs e)
+        {
+            start = true;
+            Timer.Content = $"{(int)Interface.player.NaturalDuration.TimeSpan.TotalMinutes}:{Interface.player.NaturalDuration.TimeSpan.Seconds:00}";
+            SlideMusic.Maximum = (double)Interface.player.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        private void Player_MediaEnded(object? sender, EventArgs e)
+        {
+            start = false;
+        }
+
+        private void OpenFold_Click(object sender, RoutedEventArgs e)
+        {
+            var currentFile = Interface.Open();
+            MusicList.ItemsSource = currentFile;
+            Interface.Play(0);
+        }
+
+        private void Sound_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Interface.player.Volume = Sound.Value / 100;
+        }
+
+        private void MusicList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        { 
+            Interface.Play(MusicList.SelectedIndex);
+        }
+
+        private void SlideMusic_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Interface.player.Position = TimeSpan.FromSeconds(SlideMusic.Value);
+        }
+    }   
 }
